1b99b7e751b3e9aab299e97756ed760e24df88a6
Tom(iMac) <wiredogDesign@icloud.com>
2022-10-28 13:37:49 +0100

    Added morre images & new image snippet Snippet allows formatted image block with placeholders

--- _assets/custom.js
+++ _assets/custom.js
@@ -1,7 +1,7 @@
 var trigger = document.querySelector('footer');
 var info = document.querySelector('.info');
-var imageCount = document.querySelectorAll('.thumbnail-trigger').length-1 + " images of no consequence";
-document.querySelector('header').setAttribute('data-image-counter', imageCount);
+var imageCount = document.querySelectorAll('.thumbnail-trigger').length-1;
+document.querySelector('h1').setAttribute('data-image-counter', imageCount);
 //
 function toggleClass() {
     info.classList.toggle('show-info');
