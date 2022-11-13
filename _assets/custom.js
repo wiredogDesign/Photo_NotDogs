f72b876ee1d26e845e613ba62724e352b7035b4a
Tom(iMac) <wiredogDesign@icloud.com>
2022-11-13 18:58:51 +0000

    Added radio inputs

--- _assets/custom.js
+++ _assets/custom.js
@@ -3,8 +3,12 @@ var info = document.querySelector('.info');
 var imageCount = document.querySelectorAll('.thumbnail-trigger').length-1;
 document.querySelector('h1').setAttribute('data-image-counter', imageCount);
 //
+
 function toggleClass() {
     info.classList.toggle('show-info');
 }
 trigger.addEventListener('click', toggleClass)
+//
+
+var backgroundColour = document.querySelector('input.name');
 
